7a9986b94cc120e6f82da6f47b9eb43288021cc2
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-24 12:11:52 +0900

    Implement tests for validation of Task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -18,14 +18,23 @@ RSpec.describe Task, type: :model do
 
     # it "完了期限が未入力の場合、タスクのバリデーションが無効であること" do
     it "is invalid without deadline" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: nil)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Deadline can't be blank"]
     end
 
     # it "完了期限が過去の日付の場合、タスクのバリデーションが無効であること" do
     it "is invalid if the deadline is in the past" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: Time.current - (60*60*24*365))
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Deadline must start from today."]
     end
 
     # it "完了期限が今日の日付の場合、タスクのバリデーションが有効であること" do
     it "is valid if the deadline is today" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: Time.current)
+      expect(task).to be_valid
+      expect(task.errors.any?).to be false
     end
   end
 end
